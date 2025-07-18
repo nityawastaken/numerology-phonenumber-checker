46d69f61bf6a1fa4d9c682974ad61b3b3b0672ad
nityawastaken <ruparelnitya@gmail.com>
2025-07-18 15:46:12 +0530

    DB CHANGE

--- phonechecker/settings.py
+++ phonechecker/settings.py
@@ -10,7 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -86,13 +89,15 @@ WSGI_APPLICATION = 'phonechecker.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse("postgresql://numerology_phonennumber_checker_user:CX4hHQhpqGQXWA0qVRtupXZs83P7A5Qw@dpg-d1t1nfer433s73eskiu0-a/numerology_phonennumber_checker")
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
